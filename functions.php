ee8f62fff0874bde3d2abd189bba0ed5cd8e4e22
Михаил Буслаев <buslaevnmh@yandex.ru>
2021-11-13 16:26:18 +0300

    all services translated in functions

--- functions.php
+++ functions.php
@@ -48,13 +48,14 @@ function get_data_from_avito() {
         $data[$i]['price'] = $price[$i];
     //	$data[$i]['date']=$date[$i];
         $data[$i]['description'] = $description[$i];
-        if ($data[$i]['reference'] == '' || $data[$i]['price'] > 23000 || $data[$i]['price'] < 10000) { //ORDER FILTER
+        if ($data[$i]['reference'] == '' || $data[$i]['price'] > 28000 || $data[$i]['price'] < 10000) { //ORDER FILTER
             unset($data[$i]);
         }
     }
     $data = array_values($data);//ARRAY INDEX RECOUNT
 return $data;
 }
+
 function sendMessage($parameters, $token) {
 
     $url='https://api.telegram.org/bot'.$token.'/sendMessage';
@@ -67,5 +68,70 @@ function sendMessage($parameters, $token) {
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     $output=curl_exec($curl);
     return $output;
+}
+function get_recipients($database) {
+    $get_recipients_request='select recipient_id from recipients';
+    $recipients=$database->query($get_recipients_request);
+    $recipients=$recipients->fetchAll();
+    return $recipients;
+}
+
+function put_data($pdo, $data, $recipients, $token) {
+    $put_data = $pdo->prepare("insert into avito_parsing(reference, price, description) values(?, ?, ?)");
+    $data_array_count=count($data);
+    for ($i = 0; $i < $data_array_count; $i++) {
+        $unique_check = $pdo->prepare("select reference from avito_parsing where reference = :reference");
+        $unique_check->bindParam(':reference', $data[$i]['reference'], PDO::PARAM_STR);
+        $unique_check->execute();
+        $unique_check = $unique_check->fetchAll();
+        $unique_check=$unique_check[0][0];
+    //------------------------------------CHECK OF ORDER UNIQUENESS------------------------------------------//
+        if ($unique_check==false) {
+            $put_data->bindParam(1, $data[$i]['reference']);
+            $put_data->bindParam(2, $data[$i]['price']);
+            $put_data->bindParam(3, $data[$i]['description']);
+            $put_data->execute();
+            //----------------------------------------------TELEGRAM MESSAGE SENDING--------------------------//
+            $recipients_count=count($recipients);
+            for ($j=0; $j < $recipients_count; $j++) { 
+                echo 'aaaaa ';
+                sendMessage(array("chat_id" => $recipients[$j][recipient_id], "text" => $data[$i]['reference'].' '.$data[$i]['price'].' руб'), $token);
+            }
+    
+        }
+    }
+}
+
+function telegram_messenger($token) {
+    $bot_data=file_get_contents('https://api.telegram.org/bot'.$token.'/getUpdates');
+    $bot_data=json_decode($bot_data);
+    $bot_data=json_decode(json_encode($bot_data), true);
+    //--------------------------------------CHECKING INCOME MESSAGES---------------------------------------------//
+    while ($bot_data[result][0][message][text]!=false) {
+    //--------------------------------------START AND STOP FUNCTION---------------------------------------------//
+    switch ($bot_data[result][0][message][text]) {
+    
+        case '/start':
+            $add_recipients_request='insert into recipients(recipient_id) values('.$bot_data[result][0][message][chat][id].')';
+            $pdo->query($add_recipients_request);
+            sendMessage(array("chat_id" => $bot_data[result][0][message][chat][id], "text" => 'Bot is started'), $token);
+            $offset=$bot_data[result][0][update_id]+1;
+            break;
+        case '/stop':
+            sendMessage(array("chat_id" => $bot_data[result][0][message][chat][id], "text" => 'Bot is stopped'), $token);
+            $delete_recipients_request='delete from recipients where recipient_id='.$bot_data[result][0][message][chat][id];
+            $pdo->query($delete_recipients_request);
+            $offset=$bot_data[result][0][update_id]+1;
+            break;
+        default:
+            break;
+        }
+    $offset=strval($offset);
+    $param='?offset='.$offset;
+    $bot_data=file_get_contents('https://api.telegram.org/bot'.$token.'/getUpdates'.$param);
+    $bot_data=file_get_contents('https://api.telegram.org/bot'.$token.'/getUpdates');
+    $bot_data=json_decode($bot_data);
+    $bot_data=json_decode(json_encode($bot_data), true);
+    }
 }
 ?>
--- index.php
+++ index.php
@@ -1,71 +1,16 @@
 <?php
 header('refresh: 60');//REFRESH OF QUERY
 include "functions.php";
-$token='';
+include "config.php";//$token(telegram bot) $client_id(avito api) $client_secret(avito api)
 //-------------------------------------DB CONNECT-------------------------------------------------------//
-$pdo=db_connect();
-////////////////////////////////////////TELEGRAM EXCHANGE///////////////////////////////////////////////////////
-$bot_data=file_get_contents('https://api.telegram.org/bot'.$token.'/getUpdates');
-$bot_data=json_decode($bot_data);
-$bot_data=json_decode(json_encode($bot_data), true);
-//--------------------------------------CHECKING INCOME MESSAGES---------------------------------------------//
-while ($bot_data[result][0][message][text]!=false) {
-//--------------------------------------START AND STOP FUNCTION---------------------------------------------//
-switch ($bot_data[result][0][message][text]) {
-
-    case '/start':
-        $add_recipients_request='insert into recipients(recipient_id) values('.$bot_data[result][0][message][chat][id].')';
-        $pdo->query($add_recipients_request);
-        sendMessage(array("chat_id" => $bot_data[result][0][message][chat][id], "text" => 'Bot is started'), $token);
-        $offset=$bot_data[result][0][update_id]+1;
-        break;
-    case '/stop':
-        sendMessage(array("chat_id" => $bot_data[result][0][message][chat][id], "text" => 'Bot is stopped'), $token);
-        $delete_recipients_request='delete from recipients where recipient_id='.$bot_data[result][0][message][chat][id];
-        $pdo->query($delete_recipients_request);
-        $offset=$bot_data[result][0][update_id]+1;
-        break;
-    default:
-        break;
-    }
-$offset=strval($offset);
-$param='?offset='.$offset;
-$bot_data=file_get_contents('https://api.telegram.org/bot'.$token.'/getUpdates'.$param);
-$bot_data=file_get_contents('https://api.telegram.org/bot'.$token.'/getUpdates');
-$bot_data=json_decode($bot_data);
-$bot_data=json_decode(json_encode($bot_data), true);
-}
-
-
-////////////////////////////////////////PARSING AVITO AND SENDING MESSAGES PART////////////////////////////////////
-//--------------------------------------GET RECIPIENTS LIST FROM DATABASE-------------------------------//
-$get_recipients_request='select recipient_id from recipients';
-$recipients=$pdo->query($get_recipients_request);
-$recipients=$recipients->fetchAll();
-//--------------------------------------GET DATA FROM AVITO MARKET PAGE---------------------------------//
-$data=get_data_from_avito();
-//------------------------------------PUT DATA IN TELEGRAM-----------------------------------------------//
-$put_data = $pdo->prepare("insert into avito_parsing(reference, price, description) values(?, ?, ?)");
-$data_array_count=count($data);
-for ($i = 0; $i < $data_array_count; $i++) {
-    $unique_check = $pdo->prepare("select reference from avito_parsing where reference = :reference");
-    $unique_check->bindParam(':reference', $data[$i]['reference'], PDO::PARAM_STR);
-    $unique_check->execute();
-    $unique_check = $unique_check->fetchAll();
-    $unique_check=$unique_check[0][0];
-//------------------------------------CHECK OF ORDER UNIQUENESS------------------------------------------//
-    if ($unique_check==false) {
-        $put_data->bindParam(1, $data[$i]['reference']);
-        $put_data->bindParam(2, $data[$i]['price']);
-        $put_data->bindParam(3, $data[$i]['description']);
-        $put_data->execute();
-        //----------------------------------------------TELEGRAM MESSAGE SENDING--------------------------//
-        $recipients_count=count($recipients);
-        for ($j=0; $j < $recipients_count; $j++) { 
-            sendMessage(array("chat_id" => $recipients[$j][recipient_id], "text" => $data[$i]['reference'].' '.$data[$i]['price'].' руб'), $token);
-        }
-
-    }
-}
-
+$pdo=db_connect();//COONECTING TO DATABASE
+telegram_messenger($token);//TELEGRAM EXCHANGE
+$recipients=get_recipients($pdo);//GET RECIPIENTS LIST FROM DATABASE
+$data=get_data_from_avito();//GET DATA FROM AVITO MARKET PAGE
+// echo '<pre>';
+// print_r($data);
+put_data($pdo, $data, $recipients, $token);//PUT SIGNALS IN TELEGRAM
+// $response=file_get_contents('https://avito.ru/oauth?response_type=code&client_id='.
+// $client_id.'&scope=messenger:read,messenger:write');
+// print_r($response);
 ?>
